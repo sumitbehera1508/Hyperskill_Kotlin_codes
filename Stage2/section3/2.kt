c51ca0cdf8094d735101311e86abf6efa4e73729
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-05-18 00:53:01 +0530

    init

---
+++ Stage2/section3/2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val i = 101
+    val d =3.142
+    val s = "32"
+    println(i.toLong())
+    println(i.toDouble())
+    println(d.toString())
+    println(s.toInt())
+    println(d.toLong())
+}
