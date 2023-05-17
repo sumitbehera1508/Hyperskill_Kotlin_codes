0ab29497f22059c562c37a40937c039e132dce40
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-05-18 00:46:16 +0530

    new

---
+++ Stage2/section3/1.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+fun main() {
+    // write your code here
+    val scanner =  Scanner(System.`in`)
+    var a = scanner.nextLong()
+    var b = scanner.nextLong()
+    println(a+b)
+}
