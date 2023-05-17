e2a9601388a90de30a020eca1b9ce82c5e09225d
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-05-18 02:08:52 +0530

    Create 2.kt

---
+++ Stage2/section5/2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // put your code here
+    val str ="""{
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 35,
+    "phoneNumbers": [
+        {
+            "type": "mobile",
+            "number": "123 567-7890"
+        }
+    ]
+}"""
+    println(str)
+}
